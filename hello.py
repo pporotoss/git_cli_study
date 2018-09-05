dd488b2bde6c3c9f7e6c221208c63ebb9bbc2390
권순규 <pporotoss@gmail.com>
2018-09-05 22:40:13 +0900

    GitHub for git pull by Kwon Sun-Gyu

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 # For command line git tutorial
 # First:GitHub modification
 # Second:Local repository modification
+# Third : git comit --amend
 print("Hello world")
 print("Tell Your World")
 print("Tell his world")
 print("Tell her world")
+print("Tell my world")
