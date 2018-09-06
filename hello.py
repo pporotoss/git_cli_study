3f5e19f1e51b94cb9805b1f2e9b8126e86b1827d
pporotoss <pporotoss@gmail.com>
2018-09-06 22:40:34 +0900

    Revert "GitHub for git pull by Kwon Sun-Gyu"
    This reverts commit dd488b2bde6c3c9f7e6c221208c63ebb9bbc2390.

--- hello.py
+++ hello.py
@@ -1,9 +1,7 @@
 # For command line git tutorial
 # First:GitHub modification
 # Second:Local repository modification
-# Third : git comit --amend
 print("Hello world")
 print("Tell Your World")
 print("Tell his world")
 print("Tell her world")
-print("Tell my world")
